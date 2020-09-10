78d1e3449d4f68fe8b8016cb2519a04f0330f672
monpro <monproshen@gmail.com>
2020-09-10 20:57:24 +1000

    add useLocalStorage example

---
+++ src/customhooks/UseLocalStorage.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+
+const useLocalStorage = (key, defaultValue) => {
+  const [localValue, setLocalValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : defaultValue;
+    } catch (e) {
+      console.log(e);
+      return defaultValue;
+    }
+  });
+
+  const setValue = value => {
+    try {
+      const storeValue = value instanceof Function ? value(localValue) : value;
+      setLocalValue(storeValue);
+      window.localStorage.setItem(key, JSON.stringify(storeValue))
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return [localValue, setValue];
+};
+
+/**
+ * This example is used to illustrate useLocalStorage
+ *
+const Test = () => {
+  const [name, setName] = useLocalStorage('name', 'mike');
+  const convertString = val => val.toUpperCase();
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={e => setName(convertString(e.target.value))}
+      />
+    </div>
+  );
+};
+export default Test;
+**/
