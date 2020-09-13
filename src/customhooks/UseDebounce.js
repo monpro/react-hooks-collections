05e410270d983087534bd3464b38101a5d9f6441
monpro <monproshen@gmail.com>
2020-09-13 10:35:00 +1000

    add useDebounce hook

---
+++ src/customhooks/UseDebounce.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+
+const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debounceValue
+};
