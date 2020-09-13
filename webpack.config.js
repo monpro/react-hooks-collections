74b3b14a992e4667b5b50b2dc8cb9b19b8d55861
monpro <monproshen@gmail.com>
2020-09-13 11:15:27 +1000

    add webpack .env support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv')
 module.exports = {
   entry: './src/index.tsx',
   module: {
@@ -30,6 +32,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed) // it will automatically pick up key values from .env file
     })
   ]
 };
