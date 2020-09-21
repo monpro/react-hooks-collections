a661208534997eb936dc6007f142d7cde3fcd3d9
monpro <monproshen@gmail.com>
2020-09-21 15:09:27 +1000

    add UseLockBodyScroll hook

---
+++ src/customhooks/UseLockBodyScroll.js
@@ -0,0 +1,61 @@
+import React, { useState, useLayoutEffect } from 'react';
+
+const useLockBodyScroll = () => {
+  useLayoutEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = 'hidden';
+    // enable scrolling when component unmount
+    return () => document.body.style.overflow = originalStyle;
+  }, []) // ensures effect is only run on mount and unmount
+};
+
+const Test = () => {
+  const [modelOpen, setModelOpen] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setModelOpen(true)}>Show Modal</button>
+      <Content/>
+      {modelOpen && (
+        <Model
+          title="Try scrolling"
+          content="actually you cannot"
+          onClose={() => setModelOpen(false)}
+        />
+      )}
+    </div>
+  );
+};
+
+const Model = ({title, content, onClose}) => {
+  // lock the scroll when this component is mounting
+  useLockBodyScroll();
+  return (
+    <div onClick={onClose}>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  )
+};
+
+const Content = () => {
+  const terms = [
+    'stars',
+    'birds',
+    'puppy',
+    'nature',
+    'snow',
+    'cafe',
+    'hipster',
+    'startup',
+    'salad',
+    'funny'
+  ];
+
+  const images = terms.map(term => (
+    <img src={`https://source.unsplash.com/random/800x200?${term}`} alt="" />
+  ));
+  return <div className="images">{images}</div>;
+};
+
+export default Test;
