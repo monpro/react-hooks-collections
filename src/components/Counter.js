481ee7769cbd2295e2aff63d5f741e37251fb3f4
monpro <monproshen@gmail.com>
2020-10-30 14:29:11 +1100

    add useTimeout hooks
    add UseTimeOut hooks

---
+++ src/components/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState }  from "react";
+import usePrevious from "../customhooks/UsePrevious";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const prevCount = usePrevious(count);
+
+  return (
+    <div>
+     <h1>Now: {count}, before: {prevCount}</h1>
+      <button onClick={() => setCount(count => count + 1)}>
+        add
+      </button>
+    </div>
+  )
+};
+
+
+export default Counter;
---
+++ src/customhooks/UseTimeout.js
@@ -0,0 +1,58 @@
+import {useCallback, useEffect, useRef, useState} from "react";
+import React from "react";
+
+
+const defaultOptions = {
+  cancelOnUnmount: true,
+};
+
+const useTimeOut = (cb, time, options = defaultOptions) => {
+  const opts = { ...defaultOptions, ...(options || {})};
+  const timeOut = useRef();
+  const callback = useRef(cb);
+
+  const [isCleared, setIsCleared] = useState(false);
+
+  const clear = useCallback(() => {
+    if (timeOut.current) {
+      clearTimeout(timeOut.current);
+      setIsCleared(true)
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof cb === 'function') {
+      callback.current = cb
+    }
+  }, [cb]);
+
+  useEffect(() => {
+    if (typeof time === 'number') {
+      timeOut.current = setTimeout(() => {
+        callback.current()
+      }, time)
+    }
+    return clear
+  }, [time]);
+
+  useEffect(() => {
+    return () => {
+      if(opts.cancelOnUnmount) {
+        clear()
+      }
+    }
+  });
+  return [isCleared, clear]
+};
+
+const Test = () => {
+  const [isCleared, clear] = useTimeOut(() => console.log('timeout'), 3000);
+  return (
+    <div>
+      {isCleared}
+      <button onClick={clear}>click timeout</button>
+    </div>
+  )
+};
+
+export default Test;
